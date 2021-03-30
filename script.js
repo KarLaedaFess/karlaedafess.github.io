bbf5368bd1a9b5c6bc216a09fc7bba2683b6ef72
KarlaedaFess <antoshka.yudin2000@mail.ru>
2021-03-30 19:54:05 +0900

    message

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-//empty
+const animItems = document.querySelectorAll('.text');
+
+function offset(el)
+{
+    const rect = el.getBoundingClientRect(),
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return {top: rect.top + scrollTop, left: rect.left + screenLeft}
+}
+
+if (animItems.length > 0)
+{
+    window.addEventListener('scroll', animOnScroll);
+    function animOnScroll()
+    {
+        for (let i = 0; i < animItems.length; i++)
+        {
+            const animItem = animItems[i];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+            if (animItemHeight > window.innerHeight)
+            {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight))
+            {
+                animItem.classList.add('_active');
+            }
+            else
+            {
+                if (!animItem.classList.contains('no-hide'))
+                {
+                    animItem.classList.remove('_active');
+                }
+            }
+        }
+    }
+    animOnScroll();
+}
