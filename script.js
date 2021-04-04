9d06085c92715458108e0907f66aa89f97535bfd
KarlaedaFess <antoshka.yudin2000@mail.ru>
2021-04-04 17:48:35 +0900

    message

--- script.js
+++ script.js
@@ -49,6 +49,13 @@ function hideBG()
     {
         smoke.classList.add('_hide');
     }
+    else
+    {
+        if (smoke.classList.contains('no_hide'))
+            {
+                smoke.classList.remove('_hide');
+            }
+    }
 }
 
 if (animItems.length > 0)
@@ -57,28 +64,6 @@ if (animItems.length > 0)
     animOnScroll();
 }
 ///////
-
-
-//LAZY LOADING
-const images = document.querySelectorAll("img");
-
-let imageOptions = {};
-
-let observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach((entry) => {
-        if (!entry.isIntersecting) return;
-        
-        const image = entry.target;
-        const newURL = image.getAttribute('data-src');
-        image.src = newURL;
-        observer.unobserve(image);
-    });
-}, imageOptions);
-///////
-
-images.forEach((image) => {
-    observer.observe(image);
-});
 
 window.addEventListener('scroll', hideBG);
 
