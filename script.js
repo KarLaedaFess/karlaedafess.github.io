26fcb830ad46d41468a79bb64ef396e639397e1e
KarlaedaFess <antoshka.yudin2000@mail.ru>
2021-04-07 01:57:32 +0900

    message

--- script.js
+++ script.js
@@ -1,43 +1,3 @@
-//TEXT ANIMATION ON SCROLL
-const animItems = document.querySelectorAll('.anim');
-
-function offset(el)
-{
-    const rect = el.getBoundingClientRect(),
-        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    return {top: rect.top + scrollTop, left: rect.left + screenLeft}
-}
-
-function animOnScroll()
-{
-    for (let i = 0; i < animItems.length; i++)
-       {
-            const animItem = animItems[i];
-            const animItemHeight = animItem.offsetHeight;
-            const animItemOffset = offset(animItem).top;
-            const animStart = 1;
-
-            let animItemPoint = window.innerHeight - animItemHeight / animStart;
-            if (animItemHeight > window.innerHeight)
-            {
-                animItemPoint = window.innerHeight - window.innerHeight / animStart;
-            }
-            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight))
-            {
-                animItem.classList.add('_active');
-            }
-            else
-            {
-                if (!animItem.classList.contains('no-hide'))
-                {
-                    animItem.classList.remove('_active');
-                }
-            }
-        }
-}
-//////
-
 //HIDE BACKGROUND
 /*function hideBG()
 {
@@ -59,13 +19,9 @@ function animOnScroll()
 }*/
 /////
 
-if (animItems.length > 0)
-{
-    window.addEventListener('scroll', animOnScroll);
-    setTimeout(animOnScroll, 2500);
-}
-
 //window.addEventListener('scroll', hideBG);
 
 let rellax = new Rellax('.rellax');
+
+AOS.init();
 
