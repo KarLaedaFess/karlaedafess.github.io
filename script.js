af5d381e2b9d0e8dfc889ffe9b68e274dff8499e
KarlaedaFess <antoshka.yudin2000@mail.ru>
2021-04-08 02:27:38 +0900

    message

--- script.js
+++ script.js
@@ -21,7 +21,50 @@
 
 //window.addEventListener('scroll', hideBG);
 
+window.onload = function()
+{
+    const preloader = document.querySelector('.preloader');
+    setTimeout(function() {
+        preloader.classList.add('done');
+    }, 1500);
+}
+
 let rellax = new Rellax('.rellax');
 
 AOS.init();
 
+
+//Congratulations! animation
+const anim = document.querySelector('#anim');
+const animText = anim.textContent;
+
+const splitText = animText.split("");
+anim.textContent = "";
+
+for (let i = 0; i < splitText.length; i++)
+{
+    anim.innerHTML += "<span>" + splitText[i] + "</span>";
+}
+
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick()
+{
+    const span = anim.querySelectorAll('span')[char];
+    span.classList.add("_active");
+    char++;
+
+    if (char === splitText.length)
+    {
+        complete();
+        return ;
+    }
+}
+
+function complete()
+{
+    clearInterval(timer);
+    timer = null;
+}
+////////
