e7947e05c8e6dbf0655b9b72b8af22f087844b7f
KarlaedaFess <antoshka.yudin2000@mail.ru>
2021-04-08 20:18:28 +0900

    message

--- script.js
+++ script.js
@@ -1,38 +1,7 @@
-//HIDE BACKGROUND
-/*function hideBG()
-{
-    const smoke = document.querySelector('.bg2');
-    const elem = document.querySelector('.images');
-    const posOfImages = offset(elem).top;
-    
-    if (pageYOffset * 1.5 > posOfImages)
-    {
-        smoke.classList.add('_hide');
-    }
-    else
-    {
-        if (smoke.classList.contains('no_hide'))
-            {
-                smoke.classList.remove('_hide');
-            }
-    }
-}*/
-/////
-
-//window.addEventListener('scroll', hideBG);
-
-window.onload = function()
-{
-    const preloader = document.querySelector('.preloader');
-    setTimeout(function() {
-        preloader.classList.add('done');
-    }, 1500);
-}
-
+//js libraries
 let rellax = new Rellax('.rellax');
 
 AOS.init();
-
 
 //Congratulations! animation
 const anim = document.querySelector('#anim');
@@ -46,25 +15,40 @@ for (let i = 0; i < splitText.length; i++)
     anim.innerHTML += "<span>" + splitText[i] + "</span>";
 }
 
-let char = 0;
-let timer = setInterval(onTick, 50);
-
-function onTick()
+window.onload = function()
 {
-    const span = anim.querySelectorAll('span')[char];
-    span.classList.add("_active");
-    char++;
+    const preloader = document.querySelector('.preloader');
 
-    if (char === splitText.length)
-    {
-        complete();
-        return ;
-    }
-}
+    setTimeout(function() {
+        preloader.classList.add('done');
+    }, 1500);
 
-function complete()
-{
-    clearInterval(timer);
-    timer = null;
+    setTimeout(function() {
+        let char = 0;
+        let timer = setInterval(onTick, 50);
+    
+        function onTick()
+        {
+            const span = anim.querySelectorAll('span')[char];
+            span.classList.add("_active");
+            char++;
+    
+            if (char === splitText.length)
+            {
+                complete();
+                return ;
+            }
+        }
+    
+        function complete()
+        {
+            clearInterval(timer);
+            timer = null;
+        }
+
+        const fadeIn = document.querySelector('.fadeOut');
+
+        fadeIn.classList.add('fadeIn');
+    }, 2000);
 }
 ////////
